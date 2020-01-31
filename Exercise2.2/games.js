be8175041622643f70a814bf87ce50adf9e770c5
gdeck <gdeck@mcad.edu>
2020-01-30 19:24:43 -0600

    Array with object
    Create an array with your favorite games as objects inside of the array. Each game object will represent one game.
    Each object will have the same properties. I recommend title, type, numberOfPlayers, rating, shortDescription. Please note, the naming convention of object properties is camelCase.
    You must have 3 or more games in the collection

---
+++ Exercise2.2/games.js
@@ -0,0 +1,8 @@
+// Array with 3 of my favorite games as objects, each object should have the same 4 properties
+const games = [
+    {name: 'Cards Against Humanity', numberOfPlayers: 4, rating:5, type:'Card game'},
+    {name: 'Taboo Midnight', numberOfPlayers: 4, rating:4, type:'Buzzer game'},
+    {name: 'The Game of Life Quarter Life Crisis', numberOfPlayers:2, rating:4, type:'Board game'},
+];
+
+console.log(games);
