51601d3ded0e26dc8a9f5290d85f681a9edb0b9a
gdeck <gdeck@mcad.edu>
2020-02-01 17:29:05 -0600

    storing answer and concatenated strings

--- Assignment-2A/main.js
+++ Assignment-2A/main.js
@@ -3,7 +3,7 @@ const questionArray = [
     {question: 'Where is the Great Pyramid of Giza?', answer:'Egypt'},
     {question: 'Which planet in our Solar System is known for having a ring?', answer:'Saturn'},
     {question: 'What kind of tree do acorns come from?', answer:'Oak'},
-    {question: 'What is the part of the human eye that controls the amount of light that passes through the pupil?', answer:'The iris'},
+    {question: 'What is the part of the human eye that controls the amount of light that passes through the pupil?', answer:'the iris'},
     {question: 'How many great lakes are there?', answer:'Five'},
 ];
 
@@ -11,7 +11,8 @@ const questionArray = [
 const randomQuestion = Math.round(Math.random() * questionArray.length-1);
 const firstQuestion = window.prompt(questionArray[randomQuestion].question);
 const firstAnswer = questionArray[randomQuestion].answer;
+const userFirstAnswer = firstQuestion;
 
 //Do not check the answer yet. Just fire an alert combining "You answered " and then the response and then add " The correct answer was " and then display the answer property from the object. Use concatenation to string all these strings together.
 
-console.log('firstAnswer');
+window.alert('You answered' + ' ' + userFirstAnswer + '.' + ' ' + 'The correct answer was' + ' ' + firstAnswer);
