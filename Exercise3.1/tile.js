27636c613621805aa6c9e32e5c4378c727b5bf65
gdeck <gdeck@mcad.edu>
2020-02-06 20:13:11 -0600

    custom styling

--- Exercise3.1/tile.js
+++ Exercise3.1/tile.js
@@ -4,24 +4,29 @@ function setup() {
 
 function createTile() {
     translate(0, 0);
-    fill('blue');
+// Use a square shape as the background color — don’t use your background. All other shapes should be on top of that square.
+    fill('#D5AC4E');
     rect(0, 0, 200, 200);
-    stroke('white');
-    strokeWeight(5);
-    line(0, 0, 200, 200);
-    line(200, 0, 0, 200);
+//Use good design so that this single pattern tile looks visually pleasing on its own. As you’re design it, think about what modifications to size, color, or position may lead to interesting other tiles.
     noStroke();
-    fill('yellow');
-    circle(100, 50, 50);
-    circle(50, 100, 50);
-    circle(150, 100, 50);
+    fill('#7E231D');
+    arc(100, 0, 150, 150, 0, PI);
+    arc(100, 150, 150, 150, PI, 0);
+    stroke('black');
+    noFill();
+    arc(100, 0, 170, 170, 0, PI);
+    arc(100, 150, 170, 170, PI, 0);
+    noStroke();
+    fill('#BA5624');
     circle(100, 150, 50);
     noFill();
-    stroke('purple');
-    strokeWeight(5);
-    circle(100, 100, 100);
+    stroke('#EECF6D');
+    strokeWeight('5');
+    circle(100, 150, 75);
 }
 
+//Wrap your entire ‘drawing’ with a function, so that you would have blank canvas unless you called your function. Call your pattern function in the draw function. Use the noLoop method to stop the draw loop.
 function draw() {
+    noLoop();
     createTile();
 }
