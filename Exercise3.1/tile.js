3bdbe244b9ee9e334058e4203bd8cee384ffd60f
gdeck <gdeck@mcad.edu>
2020-02-06 19:49:16 -0600

    tile

---
+++ Exercise3.1/tile.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(200, 200);
+}
+
+function createTile() {
+    translate(0, 0);
+    fill('blue');
+    rect(0, 0, 200, 200);
+}
+
+function draw() {
+    createTile();
+}
