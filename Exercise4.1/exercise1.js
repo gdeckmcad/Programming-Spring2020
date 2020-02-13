36136ae28dc1ad9978decb7e42f75886c2cb4834
gdeck <gdeck@mcad.edu>
2020-02-12 21:09:09 -0600

    Final details for 4.1

--- Exercise4.1/exercise1.js
+++ Exercise4.1/exercise1.js
@@ -5,12 +5,17 @@ const myEmptyDiv = document.querySelector('#myEmptyDiv');
 // create a new element
 const heading = document.createElement('h1');
 heading.innerHTML = 'Llama\'s communicate by humming';
-//Add event listener
+
+// adding pointer for clickable elements
+heading.style.cursor = 'pointer';
+
+// add event listener to change background color
 heading.addEventListener('click', handleHeadingClick);
-//need to attach it to the DOM with append child
+
+// need to attach heading to the DOM with append child
 myEmptyDiv.appendChild(heading);
 
 // can pass in a new or anonymous function to event listener 
 function handleHeadingClick (){
-    console.log('heading has been clicked');
+    document.body.style.backgroundColor = 'AntiqueWhite';
 }
