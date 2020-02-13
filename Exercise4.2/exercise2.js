6e1fa4516c1786dc8deea4e992695c0aff99467c
gdeck <gdeck@mcad.edu>
2020-02-13 17:45:15 -0600

    adjusting speed

--- Exercise4.2/exercise2.js
+++ Exercise4.2/exercise2.js
@@ -3,16 +3,18 @@ const rectWidth = 75;
 const rectHeight = 75;
 let rectY;
 let clickCount = 0;
+let speed;
 
 function setup() {
     createCanvas(500, 500);
     rectY = random(height - rectHeight);
+    speed = random(1, 3);
 }
 
 function draw() {
     background('SlateGray');
     drawShape();
-    rectX += 2;
+    rectX += speed;
     if(rectX > width) {
         noLoop();
         text('Your score was ' + clickCount, 175, 175);
