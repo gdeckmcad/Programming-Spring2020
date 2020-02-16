e5359fc1f2bcdd47d66c3943b5389b3cc140f30c
gdeck <gdeck@mcad.edu>
2020-02-16 17:06:49 -0600

    getting text setup

--- Assignment-4A/memory.js
+++ Assignment-4A/memory.js
@@ -2,10 +2,16 @@ let startingX = 20;
 let startingY = 90;
 let myGrid = [];
 
+//loading font
+let myFont;
+function preload () {
+    myFont = loadFont('Lato-Light.ttf');
+}
+
+//grid of cards
 function setup () {
     createCanvas(605, 569);
     background('#F8E7A9');
-    //rect(rectX, rectY, 175, 100);
     for (let k = 0; k < 4; k++) {
         for (let i = 0; i < 3; i++){
             rect(startingX, startingY, 175, 100);
@@ -15,4 +21,15 @@ function setup () {
         startingY += 115;
         startingX = 20;
     }
+}
+
+//messaging
+function draw () {
+    textFont(myFont);
+    textSize(20);
+    textAlign(CENTER, CENTER);
+    text('Charley Harper Memory Card Game', 300, 30);
+    textSize(16);
+    textAlign(CENTER,CENTER);
+    text('0/6 matches found', 300, 60);
 }
