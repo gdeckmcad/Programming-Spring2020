6effa68c15ea011d281862c545a2b049d82e0741
gdeck <gdeck@mcad.edu>
2020-02-19 21:06:29 -0600

    one bubble bouncing

--- Exercise5.2/bubble.js
+++ Exercise5.2/bubble.js
@@ -5,6 +5,16 @@ class Bubble {
         this.x = 200;
         this.y = 150;
     }
+    move() {
+        this.x = this.x + random(-5, 5); 
+        this.y = this.y + random(-5, 5);       
+    }
+    show() {
+        stroke('#0339A6');
+        strokeWeight(4);
+        noFill();
+        ellipse(this.x, this.y, 24, 24);
+    }
 }
 
 function setup() {
@@ -14,7 +24,7 @@ function setup() {
 }
 
 function draw() {
-    background(0);
-    //bubble.move();
-    //bubble.show();
+    background('#4CB1F7');
+    bubble.move();
+    bubble.show();
 }
