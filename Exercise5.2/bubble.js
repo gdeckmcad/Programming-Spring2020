9b22ead428b5b80536f7dbe34ebb05494371ffc1
gdeck <gdeck@mcad.edu>
2020-02-19 20:59:52 -0600

    setting up the bubble class

--- Exercise5.2/bubble.js
+++ Exercise5.2/bubble.js
@@ -1,7 +1,20 @@
+let bubble;
+
+class Bubble {
+    constructor() {
+        this.x = 200;
+        this.y = 150;
+    }
+}
+
 function setup() {
     createCanvas(500, 500);
+    bubble = new Bubble();
+    print(bubble.x, bubble.y);
 }
 
 function draw() {
     background(0);
+    //bubble.move();
+    //bubble.show();
 }
