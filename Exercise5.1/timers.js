763bbaa0dc999f432cceef09b28ebc3b817ffad2
gdeck <gdeck@mcad.edu>
2020-02-19 19:59:59 -0600

    setting interval

--- Exercise5.1/timers.js
+++ Exercise5.1/timers.js
@@ -1,8 +1,15 @@
+let blockX = 0;
+let blockY = 0;
+let drawTimer;
 function setup() {
     createCanvas(500, 500);
     background(0);
+    drawBlock(blockX, blockY, 255);
 }
  
-function drawBlock() {
-    rect(0, 0, 50, 50);
+function drawBlock(x, y, color) {
+    fill(color || 255);
+    rect(x, y, 50, 50);
 }
+
+drawTimer = window.setInterval(() => {}, 1000);
