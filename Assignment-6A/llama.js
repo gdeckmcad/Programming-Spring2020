957e115a00119b99fbf5aac0944631b0da694e35
gdeck <gdeck@mcad.edu>
2020-03-01 07:55:04 -0600

    facts and conditional facts

---
+++ Assignment-6A/llama.js
@@ -0,0 +1,16 @@
+new Vue ({
+    el: '#myLlamas',
+    data: {
+        factOne: 'Llamas are members of the camelid family meaning they\'re pretty closely related to vicuñas and camels',
+        factTwo: 'Llamas were first domesticated and used as pack animals 4,000 to 5,000 years ago in the Peruvian highlands',
+        factThree: 'Llamas can grow as much as 6 feet tall though the average llama between 5 feet 6 inches and 5 feet 9 inches tall',
+        factFour: 'Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight',
+        factFive: 'If you try to overload a llama with too much weight, the llama is likely to lie down or simply refuse to move',
+        factSix: 'Llamas are vegetarians and have very efficient digestive systems',
+        factSeven: 'Llama poop has almost no odor and makes for a great, eco-friendly fertilizer',
+        factEight: 'Llamas live to be about 20 years old. Though some only live for 15 years and others live to be 30 years old',
+        factNine: 'Llamas come in a range of solid and spotted colors including black, gray, beige, brown, red, and white',
+        factTen: 'The current population of llamas and alpacas in South America is estimated to be more than 7 million',
+        llamasAreAwesome: true
+    }
+})
