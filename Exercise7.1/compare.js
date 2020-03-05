a9464f92fc090daa12df66047bdc909d3db9cc90
gdeck <gdeck@mcad.edu>
2020-03-04 20:24:42 -0600

    basic lists set up for 7.1

---
+++ Exercise7.1/compare.js
@@ -0,0 +1,20 @@
+new Vue ({
+    el: '#myComparison',
+    data: {
+        llamas: [
+            {message: 'Llamas have long banana-shaped ears'},
+            {message: 'Llamas weigh 400 pounds'},
+            {message: 'Llamas make good guard animals'},
+            {message: 'Llamas have a longer face'}
+        ],
+    alpacas: [
+        {message: 'Alpacas have straight and small ears'},
+        {message: 'Alpcas weight 175 pounds'},
+        {message: 'Alpacas are a little more skittish'},
+        {message: 'Alpacas faces look smushed'}
+    ],
+    llamaLink: 'https://en.wikipedia.org/wiki/Llama',
+    alpacaLink: 'https://en.wikipedia.org/wiki/alpaca'
+}
+
+})
