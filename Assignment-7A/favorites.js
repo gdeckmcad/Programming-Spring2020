1803a0ea958347741de53da082607deae394c6b9
gdeck <gdeck@mcad.edu>
2020-03-07 20:39:38 -0600

    favorite books

--- Assignment-7A/favorites.js
+++ Assignment-7A/favorites.js
@@ -1,41 +1,41 @@
 var myBooks = [
     {
-        title:'',
-        author:'',
-        pages: '',
-        rating: '',
-        published:''
+        title:'Franny and Zooey',
+        author:'JD Salinger',
+        pages: '176 pages',
+        rating: '4.3 stars',
+        published:'May 1, 1991'
     }
 
     {
-        title:'',
-        author:'',
-        pages: '',
-        rating: '',
-        published:''
+        title:'Ghost World',
+        author:'Daniel Clowes',
+        pages: '80 pages',
+        rating: '4.3 stars',
+        published:'February 10, 2015'
     }
 
     {
-        title:'',
-        author:'',
-        pages: '',
-        rating: '',
-        published:''
+        title:'The Opposite of Loneliness',
+        author:'Marina Keegan',
+        pages: '256 pages',
+        rating: '4.4 stars',
+        published:'April 14, 2015'
     }
 
     {
-        title:'',
-        author:'',
-        pages: '',
-        rating: '',
-        published:''
+        title:'M Train',
+        author:'Patti Smith',
+        pages: '272 pages',
+        rating: '4.5 stars',
+        published:'April 1, 2016'
     }
 
     {
-        title:'',
-        author:'',
-        pages: '',
-        rating: '',
-        published:''
+        title:'1Q84',
+        author:'Haruki Murakami',
+        pages: '1,184 pages',
+        rating: '4.1 stars',
+        published:'January 22, 2013'
     }
 ]
