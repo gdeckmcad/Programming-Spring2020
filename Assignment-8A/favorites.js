728f31ac12321fb9b6151e409eba5384bed81ddf
gdeck <gdeck@mcad.edu>
2020-03-15 16:47:06 -0500

    Assignment 8A setup

---
+++ Assignment-8A/favorites.js
@@ -0,0 +1,49 @@
+var myBooks = [
+    {
+        title:'Franny and Zooey',
+        author:'JD Salinger',
+        pages: '176 pages',
+        rating: '4.3 stars',
+        published:'May 1, 1991'
+    },
+
+    {
+        title:'Ghost World',
+        author:'Daniel Clowes',
+        pages: '80 pages',
+        rating: '4.3 stars',
+        published:'February 10, 2015'
+    },
+
+    {
+        title:'The Opposite of Loneliness',
+        author:'Marina Keegan',
+        pages: '256 pages',
+        rating: '4.4 stars',
+        published:'April 14, 2015'
+    },
+
+    {
+        title:'M Train',
+        author:'Patti Smith',
+        pages: '272 pages',
+        rating: '4.5 stars',
+        published:'April 1, 2016'
+    },
+
+    {
+        title:'1Q84',
+        author:'Haruki Murakami',
+        pages: '1,184 pages',
+        rating: '4.1 stars',
+        published:'January 22, 2013'
+    }
+]
+
+var myBookList = new Vue ({
+    el: '#myBooks',
+    data : {
+        books: myBooks,
+        heading: "My Favorite Books"
+    }
+})
