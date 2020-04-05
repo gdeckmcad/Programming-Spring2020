1fb195fb126b781f0d7864233496affd81d79651
gdeck <gdeck@mcad.edu>
2020-04-05 18:22:55 -0500

    Setting up 9A and removing delete stuff

---
+++ Assignment-9A/favorites.js
@@ -0,0 +1,73 @@
+var myBooks = [
+    {
+        title:'Franny and Zooey',
+        author:'JD Salinger',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/7/72/Frannyzoey.jpg',
+        rating: '4.3 stars',
+        published:'1991-05-01'
+    },
+
+    {
+        title:'Ghost World',
+        author:'Daniel Clowes',
+        image: 'https://upload.wikimedia.org/wikipedia/en/b/b9/Ghost_world.JPG',
+        rating: '4.3 stars',
+        published:'2015-02-10'
+    },
+
+    {
+        title:'The Opposite of Loneliness',
+        author:'Marina Keegan',
+        image: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1401056149l/18143905.jpg',
+        rating: '4.4 stars',
+        published:'2015-04-14'
+    },
+
+    {
+        title:'M Train',
+        author:'Patti Smith',
+        image: 'https://upload.wikimedia.org/wikipedia/en/b/b3/M_Train_%28book%29.jpg',
+        rating: '4.5 stars',
+        published:'2016-04-01'
+    },
+
+    {
+        title:'1Q84',
+        author:'Haruki Murakami',
+        image: 'https://upload.wikimedia.org/wikipedia/en/5/59/1Q84_%28United_States_edition%29.jpg',
+        rating: '4.1 stars',
+        published:'2013-01-22'
+    }
+]
+
+var myBookList = new Vue ({
+    el: '#myBooks',
+    data : {
+        books: myBooks,
+        heading: "My Favorite Books",
+        newBookObject: {
+            title:'',
+            author:'',
+            image: '',
+            rating: '',
+            published:''
+            }
+        },
+        methods: {
+            submitHandler: () => {
+                console.log('submitted');
+                myBookList.books = myBookList.books.concat(myBookList.newBookObject);
+                myBookList.resetForm();
+            },
+            resetForm: () => {
+                myBookList.newBookObject = {
+                    title:'',
+                    author:'',
+                    image: '',
+                    rating: '',
+                    published:''
+                };
+            },
+
+        }
+})
