1d2deb9aa821822bebb4b0b94e75eb896b8a9101
gdeck <gdeck@mcad.edu>
2020-04-09 19:23:56 -0500

    starting to set up vue and component

---
+++ Exercise10.2/music-list.js
@@ -0,0 +1,23 @@
+Vue.component('streaming-track', {
+    template: `<div class="track">
+        <h3>{{track.title}}</h3>
+        <div><img src="" alt=""></div>
+        <small>{{track.artist}}</small>   
+    </div>`
+});
+
+const vm = new Vue({
+    el: "#musicApp",
+    data: {
+        tracks: []
+    },
+    mounted () {
+        axios
+            .get('./data/music-list.json')
+            .then(response => {
+                console.log('response', response);
+                vm.tracks = response.data;
+                console.log(vm.tracks);
+            });
+    }
+})
