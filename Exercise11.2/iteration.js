d92eb2095563a17ef9244da8670d80b604335950
gdeck <gdeck@mcad.edu>
2020-04-16 20:25:53 -0500

    I think I'm doing the wrong thing

--- Exercise11.2/iteration.js
+++ Exercise11.2/iteration.js
@@ -3,22 +3,22 @@ let rotateBy = 5;
 
 function setup() {
     createCanvas(600,600);
-    background(0);
+    background('#CAFFFB');
     angleMode(DEGREES);
 }
 
 function makeArm(rotateBy) {
-    let alt = Math.round(rotateBy / 360);
+    let alt = Math.round(rotateBy / 45);
     console.log(alt);
     noFill();
-    stroke(255);
+    stroke("#02066F");
     strokeWeight(1);
-    ellipse(150, 150 + alt, 150 - alt);
+    triangle(150 + alt, 150, 150, 150, 150, 150 - alt);
 }
 
 
 function draw() {
-    translate(300,300);
+    translate(150,300);
     rotate(rotateBy);
     makeArm(rotateBy);
     rotateBy += 5;
