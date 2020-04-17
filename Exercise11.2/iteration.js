4c9d5f6972aeda9a19f0cdddc50fd8edfc54af26
gdeck <gdeck@mcad.edu>
2020-04-16 19:37:22 -0500

    starting with ellipses demo

--- Exercise11.2/iteration.js
+++ Exercise11.2/iteration.js
@@ -1,34 +1,29 @@
+let angle = 0;
+let rotateBy = 5;
+
 function setup() {
-    createCanvas(200, 200);
+    createCanvas(600,600);
+    background(0);
+    angleMode(DEGREES);
 }
 
-function createTile() {
-    translate(0, 0);
-// Use a square shape as the background color — don’t use your background. All other shapes should be on top of that square.
-    fill('#D5AC4E');
-    noStroke();
-    rect(0, 0, 200, 200);
-//Use good design so that this single pattern tile looks visually pleasing on its own. As you’re design it, think about what modifications to size, color, or position may lead to interesting other tiles.
-    noStroke();
-    fill('#7E231D');
-    arc(100, 0, 150, 150, 0, PI);
-    arc(100, 150, 150, 150, PI, 0);
-    stroke('black');
-    strokeWeight('1');
+function makeArm(rotateBy) {
+    let alt = Math.round(rotateBy / 360);
+    console.log(alt);
     noFill();
-    arc(100, 0, 170, 170, 0, PI);
-    arc(100, 150, 170, 170, PI, 0);
-    noStroke();
-    fill('#BA5624');
-    circle(100, 150, 50);
-    noFill();
-    stroke('#EECF6D');
-    strokeWeight('5');
-    circle(100, 150, 75);
+    stroke(255);
+    strokeWeight(1);
+    ellipse(150, 150 + alt, 150 - alt);
 }
 
-//Wrap your entire ‘drawing’ with a function, so that you would have blank canvas unless you called your function. Call your pattern function in the draw function. Use the noLoop method to stop the draw loop.
+
 function draw() {
-    createTile();
+    translate(300,300);
+    rotate(rotateBy);
+    makeArm(rotateBy);
+    rotateBy += 5;
+}
+
+function mousePressed() {
     noLoop();
 }
