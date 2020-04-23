c3524ecdca989a8cd09844af6200e717c5ceb29b
gdeck <gdeck@mcad.edu>
2020-04-23 17:55:15 -0500

    loading in sounds

---
+++ Exercise12.1/samples.js
@@ -0,0 +1,19 @@
+var drink;
+//var fireplace;
+//var heartbeat;
+
+function preload() {
+  drink = loadSound('wav/ice-cubes-glass-daniel_simon.wav');
+  //fireplace = loadSound ('wav/crackling-fireplace_daniel-simion.wav');
+  //heartbeat = loadSound('wav/human-heartbeat-daniel_simon.wav')
+}
+
+function setup() {
+  drink.setVolume(0.1);
+  drink.play();
+}
+
+function draw() {
+  
+
+}
