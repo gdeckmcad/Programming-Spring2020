be49ddf91c8846b1dd4d0d6b46657c52d256b749
gdeck <gdeck@mcad.edu>
2020-04-23 19:07:40 -0500

    adding in all the sounds

--- Exercise12.1/samples.js
+++ Exercise12.1/samples.js
@@ -1,20 +1,32 @@
-var heartbeat;
-
 function preload() {
   heart = loadSound('wav/human-heartbeat-daniel_simon.wav')
+  stomach = loadSound('wav/Stomach Growling-SoundBible.com-164767511.wav')
+  lungs = loadSound('wav/Exhale-SoundBible.com-1772711989.wav')
 }
 
 function setup() {
   createCanvas(500, 500, WEBGL);
   amplitude = new p5.Amplitude();
-  heart.play();
 }
 
 function draw() {
   let level = amplitude.getLevel();
   strokeWeight(1);
   stroke('red');
-  rotateY(millis() / level);
-  ellipsoid(150, 150, 150);
+  circleW = level * 1000
+  circleH = level * 1000
+  ellipse(0, 0, circleW, circleH);
+}
 
+
+function playHeart() {
+  heart.play()
+}
+
+function playStomach() {
+  stomach.play()
+}
+
+function playLungs() {
+  lungs.play()
 }
