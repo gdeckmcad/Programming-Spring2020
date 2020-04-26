e98bb8a9f26de2a7e975d82d0413b221e7c7a320
gdeck <gdeck@mcad.edu>
2020-04-26 12:21:23 -0500

    preloading sound files

---
+++ Assignment-12A/instrument.js
@@ -0,0 +1,10 @@
+//preloading sound files
+
+function preload() {
+    blockOne = loadSound('sounds/blockOne.wav')
+    blockTwo = loadSound('sounds/blockTwo.wav')
+    blockThree = loadSound('sounds/blockThree.wav')
+    blockFour = loadSound('sounds/blockFour.wav')
+    blockFive = loadSound('sounds/blockFive.wav')
+    blockSix = loadSound('sounds/blockSix.wav')
+}
