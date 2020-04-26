7ee5cf96573352532732d6335c66afdb4f4f722d
gdeck <gdeck@mcad.edu>
2020-04-26 12:40:57 -0500

    grid issues
    Need to try and switch grid into p5.js vs. through html and css

--- Assignment-12A/instrument.js
+++ Assignment-12A/instrument.js
@@ -1,5 +1,4 @@
 //preloading sound files
-
 function preload() {
     blockOne = loadSound('sounds/blockOne.wav')
     blockTwo = loadSound('sounds/blockTwo.wav')
@@ -8,3 +7,24 @@ function preload() {
     blockFive = loadSound('sounds/blockFive.wav')
     blockSix = loadSound('sounds/blockSix.wav')
 }
+
+
+//setup, trying to match this up with the dimensions set in body
+function setup() {
+    createCanvas(1080, 500);
+    amplitude = new p5.Amplitude();
+}
+
+
+//drawing the shapes and getting them set up in their quadrants
+function draw() {
+    circleX = 160;
+
+    fill('#E65161');
+    noStroke();
+    rect(0, 0, 356, 250);
+    noFill();
+    strokeWeight(4);
+    stroke('#FFFFFF');
+    ellipse(175, 125, circleX, circleX);
+  }
