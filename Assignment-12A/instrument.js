9572590fd42de9372eea20715519400f03305c30
gdeck <gdeck@mcad.edu>
2020-04-26 13:05:48 -0500

    figuring out click and draw order

--- Assignment-12A/instrument.js
+++ Assignment-12A/instrument.js
@@ -11,20 +11,42 @@ function preload() {
 
 //setup, trying to match this up with the dimensions set in body
 function setup() {
-    createCanvas(1080, 500);
+    createCanvas (1080, 500);
     amplitude = new p5.Amplitude();
 }
 
+function mouseClicked () {
+    noFill();
+    noStroke();
+    rect(0, 0, 350, 250);
+    if (blockOne.isPlaying() ) {
+        blockOne.stop();
+    } else {
+        blockOne.play();
+    }
+
+    fill(0);
+    noStroke();
+    rect(350, 250, 350, 250);
+    if (blockTwo.isPlaying() ) {
+        blockTwo.stop();
+    } else {
+        blockTwo.play();
+    }
+
+}
 
 //drawing the shapes and getting them set up in their quadrants
 function draw() {
-    circleX = 160;
+    let level = amplitude.getLevel();
+    let size = level * 500;
 
+    //block 1
     fill('#E65161');
     noStroke();
-    rect(0, 0, 356, 250);
+    rect(0, 0, 350, 250);
     noFill();
     strokeWeight(4);
     stroke('#FFFFFF');
-    ellipse(175, 125, circleX, circleX);
+    ellipse(175, 125, size, size);
   }
