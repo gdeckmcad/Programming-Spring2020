0002a5f51569cba563762f075017ea8040fb5176
gdeck <gdeck@mcad.edu>
2020-05-01 07:04:29 -0500

    Adding some design

--- Exercise13.3/sketch.js
+++ Exercise13.3/sketch.js
@@ -3,32 +3,37 @@ var slider2;
 var pos;
 
 function setup() {
-    createCanvas(500, 500)
-    angleMode(DEGREES)
+    createCanvas(500, 500);
+    angleMode(DEGREES);
 
-    slider = createSlider(0, 15, 0)
-    slider2 = createSlider(0, 15, 0)
+    slider = createSlider(0, 15, 0);
+    slider.position(75,165);
+    slider2 = createSlider(0, 15, 0);
+    slider2.position(75,190);
 
     //position as float value, has decimal places
-    pos = 0.0
-    pos2 = 0.0
+    pos = 0.0;
+    pos2 = 0.0;
 
 }
 
 function draw() {
 
-    clear()
+    clear();
 
     //start our drawing from the middle of the canvas
-    translate(width/2, height/2)
+    translate(width/2, height/2);
 
-    var mySinVal = sin(pos)
-    var mySinVal2 = sin(pos2)
+    var mySinVal = sin(pos);
+    var mySinVal2 = sin(pos2);
 
-    amplified = mySinVal * 100
-    amplified2 = mySinVal2 * 100
+    amplified = mySinVal * 100;
+    amplified2 = mySinVal2 * 100;
 
+    background(0);
+    noFill();
     strokeWeight(2);
+    stroke(255);
 
     //as many times as 10 goes into 360
     for (x = 0; x <= 360; x = x + 10) {
@@ -38,6 +43,6 @@ function draw() {
 
 
     //everytime the draw function goes around sin value is giving a position that is increased by the increment and then when it gets to big it will flip back
-    pos = pos + slider.value()
-    pos2 = pos2 + slider2.value()
+    pos = pos + slider.value();
+    pos2 = pos2 + slider2.value();
 }
