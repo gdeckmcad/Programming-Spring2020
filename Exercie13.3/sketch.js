4aa02d9d9fb3f6e84c13b28df0cd91f16cc8e9fa
gdeck <gdeck@mcad.edu>
2020-04-30 20:26:56 -0500

    Ellipse sketch part one

--- Exercie13.3/sketch.js
+++ Exercie13.3/sketch.js
@@ -1,7 +1,26 @@
+var pos;
+
 function setup() {
     createCanvas(500, 500)
+    angleMode(DEGREES)
+
+    //position as float value, has decimal places
+    pos = 0.0
+
+    //determines frequency of sine value
+    inc = 3.0
 }
 
 function draw() {
-    ellipse(width/2, height/2, 50, 50)
+
+    clear()
+
+    var mySinVal = sin(pos)
+
+    amplified = mySinVal * 50
+
+    ellipse(amplified + 100, height/2, 50, mySinVal * 20)
+
+    //everytime the draw function goes around sin value is giving a position that is increased by the increment and then when it gets to big it will flip back
+    pos = pos + inc
 }
