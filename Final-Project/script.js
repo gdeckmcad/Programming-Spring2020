2f15ae824c7f4885c8fa64a3d8b5844a3d4414b4
gdeck <gdeck@mcad.edu>
2020-05-07 18:13:33 -0500

    holding on calendrific for a bit

--- Final-Project/script.js
+++ Final-Project/script.js
@@ -1,32 +1,13 @@
-const holidayEndpoint = "https://calendarific.com/api/v2/holidays";
-const apiKey = '9ef914fae7d3e6b645cfaeefe1a60a3aa1482d4e';
-// get and apiKey from calendarific https://calendarific.com/
-
 const vm = new Vue({
-  el: '#demoApp',
-  data: {
-    country: 'US',
-    year: 2019,
-    holidays: []
-  },
-  methods: {
-    getHolidays: function(){
-      let queryParams = {
-      api_key: apiKey,
-      country: vm.country,
-      year: vm.year
-      };
-      // `this` points to the vm instance
-      console.log('I have been created')
-      axios.get(holidayEndpoint, {params: queryParams})
-      .then(function(response) {
-        console.log('response from Date API', response);
-        vm.holidays = response.data.response.holidays;
-      })
-    }
-  },
-  created: function () {
-    
-  }
-})
+    el: '#demoApp',
+    data: {
 
+    },
+    mounted () {
+
+        axios.get('http://www.boredapi.com/api/activity?participants=1')
+        .then(response => {
+            console.log('response', response);
+        });
+    }
+})
