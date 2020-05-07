325efa1bf47d52336adc4eb7ee06769c22f5cc4f
gdeck <gdeck@mcad.edu>
2020-05-07 18:42:26 -0500

    Got the results to show up!
    THIS IS SO EXCITING! I couldn't get this to work in week ten

--- Final-Project/script.js
+++ Final-Project/script.js
@@ -1,11 +1,23 @@
 const vm = new Vue({
     el: '#demoApp',
     data: {
-
+        searchQuery: '',
+        results: []
+    },
+    methods: {
+        searchActivities: function() {
+            axios
+                .get('http://www.boredapi.com/api/activity', {params: {participants: vm.searchQuery}})
+                .then(response => {
+                    vm.results = response;
+                })
+        }
     },
     mounted () {
-
-        axios.get('http://www.boredapi.com/api/activity?participants=1')
+        let query = {
+            participants: '4'
+        }
+        axios.get('http://www.boredapi.com/api/activity', {params: query})
         .then(response => {
             console.log('response', response);
         });
