7b489bdcfe7a999b75263b4f7e9703ea2d9e798d
gdeck <gdeck@mcad.edu>
2020-05-07 19:06:36 -0500

    adding inspirational quote
    I'm thinking this is morphing into a self-help covid page

--- Final-Project/script.js
+++ Final-Project/script.js
@@ -1,3 +1,36 @@
+//daily quote component
+vue.component('daily-quote', {
+    template:`
+    <div>
+        <h3>{{quote.body}}</h3>
+        <p>{{quote.author}}</p>
+    </div>
+    `,
+    props: ['quote']
+});
+
+//FavQs api, inspirational quote of the day
+var baseURL = "https://favqs.com/api/"
+const quo = new Vue({
+    el: "#inspQuote",
+    data: {
+        quote: []
+    },
+    methods: {
+       getQuote: function () {
+           axios.get(baseURL + "/qotd").then(function(response){
+               console.log(response);
+               quo.quote = response.data;
+           })
+        }
+    },
+    created: function () {
+        this.getQuote();
+    }
+});
+
+
+//activity component
 Vue.component('activity-card', {
     template: `
     <div>
@@ -9,12 +42,14 @@ Vue.component('activity-card', {
     props: ['activity']
 });
 
+//bored api, generates random activities
 const vm = new Vue({
     el: '#demoApp',
     data: {
         searchQuery: '',
         results: []
     },
+//creating search query around number of participants
     methods: {
         searchActivities: function() {
             axios
